d290c0221dde7aa8c188af43cdd26d195e3d6c6b
AjinkyaChandekar <65499227+AjinkyaChandekar@users.noreply.github.com>
2022-04-04 23:11:42 +0530

    Update maven.sh

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -4,6 +4,5 @@ echo "*****************************"
 echo "*******Building Jar**********"
 echo "*****************************"
 
-WORKSPACE=/var/jenkins_home/workspace/MyFirstPipeline
+WORKSPACE=/home/jenkins-data/jenkins_home/workspace/MyFirstPipeline
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.4-ibmjava-8-alpine "$@"
-
